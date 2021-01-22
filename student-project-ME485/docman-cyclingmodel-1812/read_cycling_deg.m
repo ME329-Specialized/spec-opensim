aa099bd3aae3adaaf5bab0e3d51405b8ff33aacd
dxg2 <dxg2@stanford.edu>
2021-01-21 20:03:05 -0800

    Crank Angle update

--- student-project-ME485/docman-cyclingmodel-1812/read_cycling_deg.m
+++ student-project-ME485/docman-cyclingmodel-1812/read_cycling_deg.m
@@ -15,11 +15,14 @@ mot_headers = degrees_mot.colheaders';
 
 % first columnn of mot_data is the time series
 time = mot_data(:,1);
+% second column of mot_data is the crank angle
+crank_col = find(strcmp(mot_headers, 'bb/crank_angle/value'));
 % searches for what column in mot_data these joint angles are written
 hip_col = find(strcmp(mot_headers, 'hip_r/hip_angle_r/value'));
 knee_col = find(strcmp(mot_headers, 'knee_r/knee_angle_r/value'));
 ankle_col = find(strcmp(mot_headers, 'ankle_r/ankle_angle_r/value'));
 % accesses joint angles from mot_data
+crank_angle = mot_data(:,crank_col);
 hip_r_angle = mot_data(:,hip_col);
 knee_r_angle = mot_data(:,knee_col);
 ankle_r_angle = mot_data(:,ankle_col);
@@ -28,7 +31,7 @@ ankle_r_angle = mot_data(:,ankle_col);
 if ishandle(1)
     close(figure(1))
 end
-fig_1 = figure('Name', 'Joint Angles of Simple Model');
+fig_1 = figure('Name', 'Joint Angles vs Time');
 clf; box on; grid on; hold on;
 xlabel('Time [s]')
 xticks(0:0.25:3.5)
@@ -39,6 +42,22 @@ plot(time, knee_r_angle, 'Color', [0 0.2 1], 'DisplayName', 'Knee');
 plot(time, ankle_r_angle, 'Color', [0 0.8 0], 'DisplayName', 'Ankle');
 legend('Location', 'best', 'AutoUpdate', 'off');
 plot(time, 0*time, '--k')
+hold off;
+
+if ishandle(2)
+    close(figure(2))
+end
+fig_2 = figure('Name', 'Joint Angles vs Crank Angle');
+clf; box on; grid on; hold on;
+xlabel('Crank Angle [deg]')
+% xticks(0:0.25:3.5)
+ylabel('Joint Angle [deg]')
+set(groot,'defaultLineLineWidth',1.5)
+plot(crank_angle, hip_r_angle, 'Color', [0.8 0 0], 'DisplayName', 'Hip');
+plot(crank_angle, knee_r_angle, 'Color', [0 0.2 1], 'DisplayName', 'Knee');
+plot(crank_angle, ankle_r_angle, 'Color', [0 0.8 0], 'DisplayName', 'Ankle');
+legend('Location', 'best', 'AutoUpdate', 'off');
+plot(crank_angle, 0*time, '--k')
 hold off;
 %%
 % export joints angles to excel file for easy reading
