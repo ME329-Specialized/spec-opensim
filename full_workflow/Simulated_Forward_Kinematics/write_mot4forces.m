269838e20621c8ef5c805be12ea0deaa6b845b5e
dxg2 <dxg2@stanford.edu>
2021-02-24 03:03:09 -0800

    Consolidated Simulated Forward Kinematics and Computed Muscle Control processes into a single directory
    CMC currently can solve for the entire 3 seconds with current setup files. Motion requires several more constraints to approach a desirable solution (visually).

---
+++ full_workflow/Simulated_Forward_Kinematics/write_mot4forces.m
@@ -0,0 +1,17 @@
+function write_mot4forces(results_path, filename,version_num,nRows,nColumns,inDegrees,colheaders,data)
+% using the output data of Force Reporter, writes a .mot file (tab
+% delimited) for the constraint forces acting the midfoot
+% filename = ['external_pedal_forces', '.csv'];
+heading = {[filename,'.mot'];
+    ['version=',num2str(version_num)];
+    ['nRows=',num2str(nRows)];
+    ['nColumns=',num2str(nColumns)];
+    ['inDegrees=',inDegrees];
+    'endheader'};
+
+writecell(heading, [results_path, filename, '.txt'],'Delimiter', '\t', 'WriteMode', 'overwrite');
+writecell(colheaders, [results_path, filename, '.txt'],'Delimiter', '\t', 'WriteMode', 'append');
+writecell(num2cell(data), [results_path, filename, '.txt'],'Delimiter', '\t', 'WriteMode', 'append');
+movefile([results_path, filename, '.txt'], [results_path, filename,'.mot'])
+end
+
