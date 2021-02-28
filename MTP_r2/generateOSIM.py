2ba89802a89319a2f031e323d5d4faf7076801fc
chenseachels <cchen18@stanford.edu>
2021-02-27 17:34:12 -0800

    Making progress on getting 8e results for back up

--- MTP_r2/generateOSIM.py
+++ MTP_r2/generateOSIM.py
@@ -6,14 +6,16 @@ import numpy
 from numpy import arange
 
 # specify ranges for saddle position
-xStart = -0.15
-xEnd = -0.05
-yStart = -0.1
-yEnd = 0
+xStart = -0.11
+xEnd = -0.09
+yStart = -0.06
+yEnd = -0.04
 
 # specify file you want to modify
 defaultName = 'CMC_leg_8e_probed.osim'
 src = path.realpath(defaultName)
+print(src)
+dirsrc = '/Users/chelseachen/Documents/OpenSim/4.1/Models/OS/spec-opensim/MTP_r2/'
 
 # specify lines that need to be edited
 editLine_x = 905 # for pelvis tx
@@ -27,6 +29,9 @@ def generateOSIM():
 	for x in arange(xStart, xEnd, 0.01):
 		for y in arange(yStart, yEnd, 0.01):
 			newName = 'CMC_probed_x_' + str(x) + '_y_' + str(y) + '.osim'
+			dirName = dirsrc + 'CMC/ResultsCMC/'+'Saddle_x_' + str(x) + '_y_' + str(y)
+			os.mkdir(dirName)
+
 			# grabs the lines of the osim file
 			copyfile(src, newName)
 			newOSIM = open(newName)
